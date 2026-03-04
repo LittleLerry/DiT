04b28b72028a397ea06f2f6fe17b5fdb1a8f42fc
LittleLerry <Little_Lerry@outlook.com>
2026-03-04 14:48:54 +0800

    update

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ class tensor_image_dataset(Dataset):
     def __init__(self, path_to_tokenzied_image_tensor, path_to_labels):
         assert os.path.exists(path_to_tokenzied_image_tensor) and os.path.exists(path_to_labels)
         self.data = torch.load(path_to_tokenzied_image_tensor, weights_only=True)
+
         if path_to_labels is not None:
             self.labels = torch.load(path_to_labels, weights_only=True).to(torch.int64) # int64 for embedding layer
         else:
@@ -14,6 +15,12 @@ class tensor_image_dataset(Dataset):
         # (B, C, H, W) and (B,)
         if len(self.labels.shape) == 2:
             self.labels = self.labels.squeeze(-1)
+        
+        # filter out all gabages
+        mask = self.labels >= 0
+        self.data = self.data[mask]
+        self.labels = self.labels[mask]
+
         assert (len(self.data.shape) == 4) and (len(self.labels.shape) == 1)
         assert self.data.shape[0] == self.labels.shape[0]
         self.l = self.labels.shape[0]
--- test_api.py
+++ test_api.py
@@ -3,12 +3,8 @@ from dit import dit
 from trainer import ddp_trainer
 import torch.multiprocessing as mp
 from dataset import tensor_image_dataset
+import torch
 
-if __name__ == '__main__':
-    conf = get_general_conf({})
-    model_conf = get_model_conf(conf)
-    dataset_conf = get_dataset_conf(conf)
-
-    print("Entry training loop")
-    mp.spawn(fn=ddp_trainer, args=(conf, dit, tensor_image_dataset, model_conf, dataset_conf), nprocs=conf["world_size"], join=True) # To be trained on 8*H800
-    # create detailed log method
+t = torch.load("./data/l.pt")
+print(torch.max(t))
+print(torch.min(t))
